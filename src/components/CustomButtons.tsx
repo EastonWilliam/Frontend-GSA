6ba805da30e94ae688fbc40ffa864074fd26e13c
EastonWilliam <albertokimasa@gmail.com>
2025-11-28 08:20:38 -0500

    GSA Initial Project

---
+++ src/components/CustomButtons.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement, useState } from 'react'
+
+import type { BoxProps, SxProps, Theme } from '@mui/material'
+import { useTheme, Box, Typography, Button, Stack, TextField } from '@mui/material'
+
+
+interface InputButtonProps {
+  price: string | number,
+  label: string,
+  prefix: string
+  sx?: SxProps
+}
+
+export const InputButton = ({ price, label, prefix, sx = {}, ...props }: InputButtonProps) => {
+  const [budget, setBudget] = useState(price);
+  const update = () => {
+    setBudget(budget)
+  }
+  return (
+    <Stack
+      direction={'row'}
+      justifyContent={'space-between'}
+      sx={{
+        ...sx
+      }}
+      {...props}>
+      <Box display={'flex'} flexDirection={'row'}>
+        <Typography>$</Typography>
+        <TextField value={budget} onChange={update}></TextField>
+      </Box>
+      <Typography>{label}</Typography>
+    </Stack>
+  )
+
+}
+
+
